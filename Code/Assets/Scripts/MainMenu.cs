903d6c269c814c7b2e521bd6abdea2792f24f157
MohamedYahya-M <yahya23mohamed@gmail.com>
2024-09-28 17:38:45 +0530

    Initial commit

---
+++ Code/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    
+    void Start()
+    {
+        
+    }
+
+   
+    void Update()
+    {
+        
+    }
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("LevelScene");
+    }
+
+    public void BackToMainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void retry()
+    {
+        SceneManager.LoadScene("LevelScene");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#endif
+    }
+}
